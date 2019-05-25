0ee1334baccf1fabf961835cd30265108e84e9e8
Juan Martín García <hello@juangarcia.com.ar>
2019-05-25 17:42:37 -0300

    Filter and remove button functionality

---
+++ src/components/ExpenseListFilters.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { setTextFilter } from "../actions/filters";
+
+const ExpenseListFilters = props => {
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        value={props.filters.text}
+        onChange={e => {
+          props.dispatch(setTextFilter(e.target.value));
+        }}
+      />
+    </React.Fragment>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    filters: state.filters
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseListFilters);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ description, amount, createdAt }) => {
+const ExpenseListItem = ({
+  dispatch,
+  id,
+  description,
+  amount,
+  createdAt,
+  props
+}) => {
   return (
     <li>
       <h3>{description}</h3>
       <h5>
         {amount} - {createdAt}
       </h5>
+      <button
+        onClick={() => {
+          dispatch(removeExpense({ id }));
+        }}
+      >
+        Remove
+      </button>
     </li>
   );
 };
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);

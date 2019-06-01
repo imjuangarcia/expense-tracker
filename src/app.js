6df938e038195411afe4f975c08c4035e8b13716
Juan Martín García <hello@juangarcia.com.ar>
2019-06-01 12:42:21 -0300

    Remove hard coded data for store

--- src/app.js
+++ src/app.js
@@ -11,17 +11,6 @@ import "./styles/style.scss";
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas Bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
-
-const storeState = store.getState();
-const visibleExpenses = getVisibleExpenses(
-  storeState.expenses,
-  storeState.filters
-);
-
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>

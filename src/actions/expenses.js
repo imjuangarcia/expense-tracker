284e146f00ca4b3bf300207a4533ec5fd5bfa25c
Juan Martín García <hello@juangarcia.com.ar>
2019-06-05 19:08:44 -0300

    Setup remove expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -40,6 +40,17 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return dispatch => {
+    return database
+      .ref(`expenses/${id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense({ id }));
+      });
+  };
+};
+
 export const setExpenses = expenses => ({
   type: "SET_EXPENSES",
   expenses
